8126d6917093c256b7185114de3b9fb898117390
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-09-09 15:12:50 +0500

    Customers Loop

--- python/generate_data.py
+++ python/generate_data.py
@@ -24,3 +24,22 @@ property_area = ["Urban", "Semiurban", "Rural"]
 loan_terms = [120, 180, 240, 300, 360] # months
 credit_hist = [0, 1]
 
+# 1) Customers
+
+customers = []
+for cid in customer_ids:
+    app_income = random.randint(1500, 30000) # PKR/month scale
+    coapp_income = random.choice([0, random.randint(0, 15000)])
+    customers.append({
+        "customer_id": cid,
+        "gender": random.choice(genders),
+        "married": random.choice(married),
+        "dependents": random.choice(dependents),
+        "education": random.choice(education),
+        "self_employed": random.choice(self_employed),
+        "applicant_income": app_income,
+        "coapplicant_income": coapp_income,
+        "loan_history_flag": random.choice([0,1]) # extra feature
+    })
+
+customers_df = pd.DataFrame(customers)
