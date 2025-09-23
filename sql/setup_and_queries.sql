38da7bb9e310564195c558736da66ae8945f3c71
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-09-23 16:14:23 +0500

    Customers table

--- sql/setup_and_queries.sql
+++ sql/setup_and_queries.sql
@@ -2,3 +2,16 @@
 IF OBJECT_ID('customers') IS NOT NULL DROP TABLE customers;
 IF OBJECT_ID('loans') IS NOT NULL DROP TABLE loans;
 
+-- Customers table
+CREATE TABLE customers(
+    customer_id VARCHAR(10) PRIMARY KEY,
+    gender VARCHAR(10),
+    married VARCHAR(5),
+    dependents VARCHAR(5),
+    education VARCHAR(20),
+    self_employed VARCHAR(5),
+    applicant_income INT,
+    coapplicant_income INT,
+    loan_history_flag BIT
+)
+
