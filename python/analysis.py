65a1224004bc2f67b61ecffa1503affffad8b934
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-02 16:00:32 +0500

    Clean & Merge the Data

--- python/analysis.py
+++ python/analysis.py
@@ -12,3 +12,19 @@ loans = pd.read_csv("../data/loans.csv")
 print("Customers shape:", customers.shape)
 print("Loans shape:", loans.shape)
 
+# ==================
+# 2. Clean Data
+# ==================
+# Standardize column names (lowercase)
+customers.columns = customers.columns.str.lower()
+loans.columns = loans.columns.str.lower()
+
+# Ensure numeric columns
+loans['loan_amount'] = pd.to_numeric(loans['loan_amount'], errors = 'coerce')
+customers['applicant_income'] = pd.to_numeric(customers['applicant_income'], errors = 'coerce')
+
+# Merge
+df = loans.merge(customers, on='customer_id', how='left')
+
+print ("Merged dataset shape:", df.shape)
+
